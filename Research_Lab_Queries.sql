9e8c900df51bc40e0e84a3206101dd71c6087ec7
AR2542 <ar2542@njit.edu>
2025-12-06 21:38:56 -0500

    Update Research_Lab_Queries.sql
    Added insert values to Lab Member, Student, Faculty and External Collaborator

--- Research_Lab_Queries.sql
+++ Research_Lab_Queries.sql
@@ -2,7 +2,6 @@ create database if not exists Research_Lab;
 
 use Research_Lab;
 
-#Drop Table Statements
 drop table if exists Authorship;
 drop table if exists Publication;
 
@@ -11,8 +10,8 @@ drop table if exists Funding;
 drop table if exists Project_Grant;
 drop table if exists Project;
 
-drop table if exists Equipment;
 drop table if exists Equipment_Usage;
+drop table if exists Equipment;
 
 drop table if exists Student;
 drop table if exists Faculty;
@@ -20,7 +19,6 @@ drop table if exists External_Collaborator;
 drop table if exists Mentorship;
 drop table if exists Lab_Member;
 
-#Create Table Statements
 create table Lab_Member (
     Member_ID int Primary Key,
     Member_Name Varchar(255) not null,
@@ -114,7 +112,7 @@ create table Funding(
     Grant_ID int not null,
     primary key (Project_ID, Grant_ID),
     foreign key (Project_ID) references Project(Project_ID),
-    foreign key (Grant_ID) references Grant(ID)
+    foreign key (Grant_ID) references Project_Grant(ID)
 );
 
 create table Publication (
@@ -135,8 +133,57 @@ create table Authorship(
 );
 
 insert into Lab_Member(Member_ID, Member_Name, Member_Type, Join_Date) values 
-    (100, 'Abhinav Ramesh', 'Student', '2019-09-30'),
-    (101, 'Dimitre Theodoratos', 'Faculty', '2003-05-17'),
-    (102, 'Bill Mccann', 'External Collaborator', '1998-10-12');
+    (100, 'John', 'Student', '2019-09-30'),
+    (101, 'Smith', 'Faculty', '2018-05-17'),
+    (102, 'Franklin', 'External Collaborator', '2019-10-12'),
+    (103, 'Wong', 'Student', '2018-11-01'),
+    (104, 'Alicia', 'Faculty', '2003-05-17'),
+    (105, 'Zelaya', 'External Collaborator', '1998-10-12'),
+    (106, 'Jennifer', 'Student', '2019-09-30'),
+    (107, 'Wallace', 'Faculty', '2003-05-17'),
+    (108, 'Ramesh', 'External Collaborator', '1998-10-12'),
+    (109, 'Narayan', 'Student', '2019-09-30'),
+    (110, 'Joyce', 'Faculty', '2003-05-17'),
+    (111, 'English', 'External Collaborator', '1998-10-12'),
+    (112, 'Ahmad', 'Student', '2019-09-30'),
+    (113, 'James', 'Faculty', '2003-05-17'),
+    (114, 'Borg', 'External Collaborator', '1998-10-12'),
+    (115, 'Michael', 'Student', '2019-09-30'),
+    (116, 'Ron', 'Student', '2019-09-30'),
+    (117, 'Wade', 'Student', '2019-09-30'),
+    (118, 'Trace', 'Student', '2019-09-30'),
+    (119, 'Rachel', 'Student', '2019-09-30');
+    
+select count(*) from Lab_Member;
+    
+insert into Student(Member_ID, Student_Number, Academic_Level, Major) values
+    (100, 1000, 'Freshman', 'Computer Science'),
+    (103, 1001, 'Sophomore', 'Mathematics'),
+    (106, 1002, 'Freshman', 'Accounting'),
+    (109, 1003, 'Senior', 'Computer Science'),
+    (112, 1004, 'Junior', 'Biology'),
+    (115, 1005, 'Sophomore', 'Chemistry'),
+    (116, 1006, 'Junior', 'Accounting'),
+    (117, 1007, 'Freshman', 'Chemistry'),
+    (118, 1008, 'Senior', 'Mathematics'),
+    (119, 1009, 'Junior', 'Computer Science');
 
-select * from Lab_Member;
+select count(*) from Student;
+
+insert into Faculty(Member_ID, Department) values 
+    (101, 'Theoretical Chemistry'),
+    (104, 'Mechanical and Current Physics'),
+    (107, 'Calculus'),
+    (110, 'Psychology and Arts'),
+    (113, 'Commerce');
+    
+select count(*) from Faculty;
+    
+insert into External_Collaborator(Member_ID, Institutional_Affiliation, Biography) values 
+    (102, 'NJIT', 'He has a Masters in Computer Science and has over 8 years of experience as a professor in MIT.'),
+    (105, 'Rutgers', 'She has done a Doctorate and has contributed a lot in the field of programming.'),
+    (108, 'Harvard', 'He has worked in the accounting field for over 30 years and is currently a professor at Harvard for over 5 years.'),
+    (111, 'NJIT', 'External Collaborator'),
+    (114, 'NJIT', 'External Collaborator');
+
+select count(*) from External_Collaborator;
